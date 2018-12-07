85fe14451161029f6be8895ad0617bda9f452cd5
zxcweb <1028150816@qq.com>
2018-12-07 15:11:09 +0800

    version error modify(rebuild my project)

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
 
 // 使用body-parser中间件
 app.use(bodyParser.urlencoded({ extended: false }));
